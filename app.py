6ad624530f33c04720a81ccba5cd85b103914db3
Kauan José <kauanjose1255@gmail.com>
2024-05-10 22:16:28 -0300

    try

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import configure
 from database import db
 
-def create_app():
+def create_app(*args, **kwargs):
     app = Flask(__name__)
     configure(app)
     return app
